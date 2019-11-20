4a608f797fa95fe469299eb020a2dcd75409fa3a
Simon Crouzet <simon.crouzet@laposte.net>
2019-11-20 13:15:20 +0100

    RED - Create a test that verify if the method getWinner() return the player 1 when this player has enough point to win

--- src/test/java/Model/GameTest.java
+++ src/test/java/Model/GameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class GameTest {
     private Game SUT;
@@ -17,5 +18,18 @@ class GameTest {
     void initializeTest() {
         assertNotNull(SUT.getPlayer1());
         assertNotNull(SUT.getPlayer2());
+    }
+
+
+    /**
+     * Here we mock a player who have enough points to win the game. We set this mockPlayer in our Game, and we
+     * check if the method getWinner() will correctly return this player
+     */
+    @Test
+    void getWinnerReturnPlayer1IfWinning() {
+        Player mock = mock(Player.class);
+        when(Player.getScore()).thenReturn(SUT.getWinningScoreLimit());
+        SUT.setPlayer1(mock);
+        assertEquals(SUT.getPlayer1(), SUT.getWinner());
     }
 }
