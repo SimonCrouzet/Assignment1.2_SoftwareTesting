0357e951916eb1264a80a4decb112c362631f1af
chesiak <ola.chesiak@gmail.com>
2019-11-20 12:53:24 +0100

    GREEN - added a new class Player and modified Game constructor to pass the test

--- src/main/java/Model/Game.java
+++ src/main/java/Model/Game.java
@@ -1,7 +1,19 @@
 package Model;
 
 public class Game {
+    private Player player1;
+    private Player player2;
 
     public Game() {
+        player1 = new Player();
+        player2 = new Player();
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
     }
 }
