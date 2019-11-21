6c478b24aa8b54ef1a2e6b50a0013ca231f09e3b
chesiak <ola.chesiak@gmail.com>
2019-11-21 13:42:09 +0100

    REFACTOR - changed method to get the player's number.

--- src/main/java/View/ConsoleMessages.java
+++ src/main/java/View/ConsoleMessages.java
@@ -5,6 +5,6 @@ import Model.Player;
 public class ConsoleMessages {      // class with messages that we would print to the console
 
     public void welcomeMessage() {
-        System.out.print("Welcome to the BIG FIGHT game!");
+        System.out.print("Welcome to the BIG FIGHT game!\n");
     }
 }
