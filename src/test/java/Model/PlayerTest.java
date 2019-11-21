a7cfadbeb107ff58bbed1d253f84fd18c9994c8c
chesiak <ola.chesiak@gmail.com>
2019-11-21 12:34:12 +0100

    RED - player number is 2 and the method returned 1.

--- src/test/java/Model/PlayerTest.java
+++ src/test/java/Model/PlayerTest.java
@@ -14,10 +14,18 @@ class PlayerTest {
     }
 
     @Test
-    void returnPlayerNumberTest() {
-
+    void returnPlayerNumberTestShouldReturnOne() {
+        SUT.setPlayerNumber(1);
         int actual = SUT.getPlayerNumber();
 
         assertEquals(1, actual);
+    }
+
+    @Test
+    void returnPlayerNumberShouldReturnTwo() {
+        SUT.setPlayerNumber( 2 );
+        int actual = SUT.getPlayerNumber();
+
+        assertEquals(2, actual);
     }
 }
