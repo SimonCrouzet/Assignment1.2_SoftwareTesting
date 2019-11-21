1681da5c31aaaef32e0492cc97897d3248fab49b
chesiak <ola.chesiak@gmail.com>
2019-11-21 11:46:17 +0100

    GREEN - minimal implementation so the test passes. Added welcome message.

---
+++ src/main/java/View/ConsoleMessages.java
@@ -0,0 +1,9 @@
+package View;
+
+public class ConsoleMessages {
+
+    public void welcomeMessage() {
+        System.out.print("Welcome to the BIG FIGHT game!" +
+                "\nChoose how many rounds you want to play.\n");
+    }
+}
