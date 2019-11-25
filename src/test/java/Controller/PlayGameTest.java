bad49f4bc1d8f99e7749269b45ff00c257e4a7d4
Simon Crouzet <simon.crouzet@laposte.net>
2019-11-26 00:53:33 +0100

    RED - Create a test that verify if we print the winner. Adapt the setUp() method for that

--- src/test/java/Controller/PlayGameTest.java
+++ src/test/java/Controller/PlayGameTest.java
@@ -1,23 +1,36 @@
 package Controller;
 
+import Model.Game;
+import Model.Player;
 import View.ConsoleMessages;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
 class PlayGameTest {
     private PlayGame SUT;
+    private Game mockGame;
+    private Player mockPlayer;
+    private ConsoleMessages mockConsole;
 
     @BeforeEach
     void setUp() {
         SUT = new PlayGame();
+
+        mockGame = mock(Game.class);
+        mockPlayer = mock(Player.class);
+
+        SUT.setGame(mockGame);
+
+        when(mockGame.getWinner()).thenReturn(mockPlayer);
+        when(mockPlayer.getPlayerNumber()).thenReturn(1);
+        when(mockPlayer.getScore()).thenReturn(3);
     }
 
     @Test
@@ -27,7 +40,7 @@ class PlayGameTest {
 
     @Test
     void shouldBePolite() {
-        ConsoleMessages mockConsole = new ConsoleMessages();
+        mockConsole = new ConsoleMessages();
         mockConsole = Mockito.spy(mockConsole);
 
         SUT.setConsole(mockConsole);
@@ -36,5 +49,17 @@ class PlayGameTest {
 
         verify(mockConsole).welcomeMessage();
         verify(mockConsole).goodbyeMessage();
+    }
+
+    @Test
+    void shouldPrintGameWinner() {
+        mockConsole = new ConsoleMessages();
+        mockConsole = Mockito.spy(mockConsole);
+
+        SUT.setConsole(mockConsole);
+
+        SUT.start();
+
+        verify(mockConsole).printGameWinner(mockPlayer);
     }
 }
