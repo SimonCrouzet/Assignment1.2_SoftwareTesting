d471a30a5b1a8d33f948b6505f69f2b8eb93211d
Simon Crouzet <simon.crouzet@laposte.net>
2019-11-26 00:40:31 +0100

    GREEN - Update the start() method to pass the tests. Also create a constructor and a setter

--- src/main/java/Controller/PlayGame.java
+++ src/main/java/Controller/PlayGame.java
@@ -1,12 +1,31 @@
 package Controller;
 
 import Model.Game;
+import View.ConsoleMessages;
 
 public class PlayGame {
     private Game game;
+    private ConsoleMessages console;
+
+    public PlayGame(Game game, ConsoleMessages console) {
+        this.game = game;
+        this.console = console;
+    }
+
+    public PlayGame() {
+        game = new Game();
+        console = new ConsoleMessages();
+    }
 
     public boolean start() {
-        game = new Game();
+
+        console.welcomeMessage();
+        console.goodbyeMessage();
+
         return true;
+    }
+
+    public void setConsole(ConsoleMessages console) {
+        this.console = console;
     }
 }
